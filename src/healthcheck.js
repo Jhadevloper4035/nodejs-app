9861d6007c12d7a55233f856d09b5f759421ea2e
Jhadevloper4035 <navrojjha21@gmail.com>
2026-02-25 16:37:01 +0800

    update header and footer

--- src/healthcheck.js
+++
@@ -1,8 +0,0 @@
-const http = require("http");
-
-const req = http.request(
-  { hostname: "localhost", port: process.env.PORT || 3000, path: "/health", method: "GET" },
-  (res) => process.exit(res.statusCode === 200 ? 0 : 1)
-);
-req.on("error", () => process.exit(1));
-req.end();
